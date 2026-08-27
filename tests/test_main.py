f1d1c2343701cd6862b60874b1453b4c62d2848d
Lucky6566 <lakshmanmude151110@gmail.com>
2026-08-27 17:03:40 +0530

    Add API tests

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+﻿from fastapi.testclient import TestClient
+from backend.main import app
+
+client = TestClient(app)
+
+
+def test_root_endpoint():
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_openapi_endpoint():
+    response = client.get("/openapi.json")
+    assert response.status_code == 200
